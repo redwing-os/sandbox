5cc4aa14363a522978abd024fecce2913c960b2b
Sinan Ulkuatam <sku3hh@gmail.com>
2023-12-29 13:54:16 -0500

    .

--- regex.sh
+++ regex.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Load the .env file and extract the REPLACEMENT_IP
+if [ -f ".env" ]; then
+    export $(grep -v '^#' .env | xargs)
+else
+    echo ".env file not found. Exiting."
+    exit 1
+fi
+
 # Use the environment variable for the replacement IP
 replacement_ip=$REPLACEMENT_IP
 
