3f341a7347bec673ab6e7894b26bb7f019ca4674
Sinan Ulkuatam <sku3hh@gmail.com>
2024-01-07 18:36:10 -0500

    .

---
+++ catalyst.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define the Docker image name
+DOCKER_IMAGE="helloredwing/catalyst"
+HOST="localhost"
+PORT="8080"
+
+# Pull the Docker image
+echo "Pulling Docker image: $DOCKER_IMAGE..."
+docker pull $DOCKER_IMAGE
+
+# Run the Docker container
+echo -e "\033[1;44m Running Docker container for $DOCKER_IMAGE on $HOST:$PORT \033[0m"
+docker run -d -p 8080:80 $DOCKER_IMAGE
